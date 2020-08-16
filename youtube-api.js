0ae8a9bc599d70affe4b96e80ebed435bdf402b8
Thomas Clark <thomas.w.clark.22@dartmouth.edu>
2020-08-15 18:30:08 -0700

    initializing repo

---
+++ youtube-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'AIzaSyBmfyCBSF9qBuuOYpKnN5sXBlEIJlf4kPk';
+
+const youtubeSearch = (term) => {
+  const params = {
+    part: 'snippet',
+    key: API_KEY,
+    q: term,
+    type: 'video',
+  };
+  return new Promise((resolve, reject) => {
+    axios.get(API_URL, { params })
+      .then((response) => {
+        resolve(response.data.items);
+      })
+      .catch((error) => {
+        console.log(`youtube api error: ${error}`);
+        reject(error);
+      });
+  });
+};
+
+export default youtubeSearch;
